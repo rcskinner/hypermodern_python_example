92cdc0fc850a2772fc10bc1151aee1f117858e45
Ryan Skinner <rskinner593@gmail.com>
2020-09-25 14:50:51 -0500

    Built a unit test to confirm that the wikipedia call is working

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+# tests/test_console.py
+import click.testing 
+import pytest 
+
+from hypermodern_python_example import console
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds():
+    runner = click.testing.CliRunner()
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
