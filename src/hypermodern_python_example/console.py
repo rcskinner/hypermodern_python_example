65e0be752b82eb3f1afde8ac1d7125b496474156
Ryan Skinner <rskinner593@gmail.com>
2020-09-25 22:21:26 -0500

    Added optional language argument to the console command

--- src/hypermodern_python_example/console.py
+++ src/hypermodern_python_example/console.py
@@ -10,11 +10,19 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
-    """The hypermodern python project."""
+def main(language):
+    """The hypermodern Python project."""
+    data = wikipedia.random_page(language=language)
 
-    data = wikipedia.random_page()
     title = data["title"]
     extract = data["extract"]
 
--- tests/test_console.py
+++ tests/test_console.py
@@ -39,3 +39,10 @@ def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hypermodern_python.wikipedia.random_page")
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
