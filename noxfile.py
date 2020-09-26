f1a8f2122f1fa4acb4f4e782de53c1eed4b50f7d
Ryan Skinner <rskinner593@gmail.com>
2020-09-26 07:24:27 -0500

    Updated nox to work with --dev / --no_dev arguments, allowing each session to only use the packages/dependencies they need

--- noxfile.py
+++ noxfile.py
@@ -8,27 +8,53 @@ nox.options.sessions = "lint", "tests", "safety"
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.8"])
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}"
+            external=True
+        )
+    session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+
+# Black session to modify the code to match standard guide
+@nox.session(python="3.8")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
+
 
 
 # Linter argument that checks for stylistic / complexity issues
 @nox.session(python=["3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
     session.run("flake8", *args)
 
-
-@nox.session(python=["3.8"])
+# Nox session that runs the unit tests for the 3.8 version
 def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
+    session.run("pytest", *args)
 
-
+# Nox session that runs the safety/vulnerability checks
 @nox.session(python="3.8")
 def safety(session):
     with tempfile.NamedTemporaryFile() as requirements:
@@ -41,5 +67,5 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
