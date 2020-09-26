f02581382e14b6c801ef3c83330fd6a1c29cc933
Ryan Skinner <rskinner593@gmail.com>
2020-09-26 07:10:49 -0500

    Added the flake8 safety feture to check for vulnerabilities

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,10 @@
 # noxfile.py
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+
+nox.options.sessions = "lint", "tests", "safety"
 locations = "src", "tests", "noxfile.py"
 
 
@@ -16,7 +19,7 @@ def black(session):
 @nox.session(python=["3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
     session.run("flake8", *args)
 
 
@@ -24,3 +27,19 @@ def lint(session):
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
