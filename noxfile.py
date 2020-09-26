5b052d013232342600097fab2d5312ed026309b7
Ryan Skinner <rskinner593@gmail.com>
2020-09-25 22:41:02 -0500

    Added linting and black arguments to fix formatting

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,24 @@
 # noxfile.py
 import nox
 
+nox.options.sessions = "lint", "tests"
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
+
+
+# Linter argument that checks for stylistic / complexity issues
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8", "flake8-black")
+    session.run("flake8", *args)
+
 
 @nox.session(python=["3.8"])
 def tests(session):
